01d7d592763b85073c76060ba618a59bd9a58b22
Venkatraman B <vbala038@uottawa.ca>
2021-09-13 14:01:40 -0700

    Add files via upload

---
+++ src/mobility/contract/IMobility.cc
@@ -0,0 +1,34 @@
+/* -*- mode:c++ -*- ********************************************************
+ * file:        IMobility.cc
+ *
+ * author:      Levente Meszaros
+ *
+ * copyright:   (C) 2004 Telecommunication Networks Group (TKN) at
+ *              Technische Universitaet Berlin, Germany.
+ *
+ *              (C) 2005 Andras Varga
+ *
+ *              This program is free software; you can redistribute it
+ *              and/or modify it under the terms of the GNU General Public
+ *              License as published by the Free Software Foundation; either
+ *              version 2 of the License, or (at your option) any later
+ *              version.
+ *              For further information see file COPYING
+ *              in the top level directory
+ ***************************************************************************
+ * part of:     framework implementation developed by tkn
+ *************************************************************************/
+
+#include "inet/mobility/contract/IMobility.h"
+//#ifdef EXPORT
+//    #define DLL __declspec(dllexport)
+//#else
+//    #define DLL __declspec(dllimport)
+//#endif
+
+namespace inet {
+
+//static simsignal_t IMobility::mobilityStateChangedSignal = cComponent::registerSignal("mobilityStateChanged");
+
+} // namespace inet
+
